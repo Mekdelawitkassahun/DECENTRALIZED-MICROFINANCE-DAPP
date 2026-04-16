648a69d5e6ef2658f03b295a631c8a87231cc143
Mekdelawitkassahun <mekdikassahun2360@gmail.com>
2026-04-16 19:30:43 +0300

    hardhat.config added

---
+++ frontend-config.js
@@ -0,0 +1,11 @@
+
+// Ethiopian Microfinance DApp - Sepolia Configuration
+export const CONTRACT_ADDRESS = "undefined";
+export const NETWORK_ID = 11155111;
+export const NETWORK_NAME = "sepolia";
+export const RPC_URL = "https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+export const EXPLORER_URL = "https://sepolia.etherscan.io";
+export const CHAIN_NAME = "Sepolia Testnet";
+export const CURRENCY_SYMBOL = "ETH";
+export const BLOCK_EXPLORER = "https://sepolia.etherscan.io";
+  
